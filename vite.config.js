f04d45c23e311c7c994bbb15fe980d22fba4d65b
Donovan Crewe <donnycrash@gmail.com>
2025-06-18 15:36:37 +0200

    Updates config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/YOUR_REPO_NAME/",
+  base: "/jules-test/",
   plugins: [react()],
 });
